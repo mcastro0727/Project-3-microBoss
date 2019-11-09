54c1f7ecbcea3b31f9f6fe8d335ada2459235b68
Rafael Galvez <rfgalvez@msn.com>
2019-11-09 13:47:55 -0500

    commit for pull

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,18 +1,17 @@
- const db = require("../models");
+const db = require("../models");
 
 // Defining methods for the ProjectsController
 controller = {
   findAll: function(req, res) {
     console.log("findall");
     // res.send("/api : Get");
-    db.Project
-    .find(req.query)
-    .sort({ date: -1 })
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
+    db.Project.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    console.log("create")
+    console.log("create");
     res.send("/api/projects : Post");
   },
   findById: function(req, res) {
