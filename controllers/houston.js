ddf2aa00d199edae0aeee9c45c85706bb1e5fc04
James Appello <jamesappello@gmail.com>
2019-11-26 18:41:38 -0500

    move over from sandbox to project

--- controllers/houston.js
+++ controllers/houston.js
@@ -1,48 +1,99 @@
-const db = require("../models");
+const { Project, Profile } = require('../models');
 
-// Defining methods for the ProjectsController
-controller = {
-  // SIGMA(get)
-  findAll: function(req, res) {
-    db.Project.find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    console.log("create");
-    res.send("/api : Post");
-  },
-  // GET / the /:id for the api
-  findById: function(req, res) {
-    db.Project
-    .findById(req.params)
-    .exec()
-    .then(result => {
-      console.log(result);
-      res.status(200).json(result);
-    }).catch(err => res.status(500).json(err));
-    res.send("/api/:id : Get");
-  },
-  update: function(req, res) {
-    db.Project
-      .findByIdAndUpdate(req.params)
-      .exec()
-      .then(resu => {
-        console.log(resu);
-        res.status(200).json(resu);
-      }).catch(err => res.status(500).json(err));
-    res.send("/api/:id : Put");
-  },
-  remove: function(req, res) {
-    const id = req.params.id;
-    db.Project
-      .remove({ _id: id })
-      .exec()
-      .then(result => {
-        res.status(200).json(result)
-      }).catch(err => res.status(500).json(err));
-    res.send("/api/:id : Delete");
-  }
+
+module.exports = {
+    // PROJECT METHODS
+    findAllProjects: function(req,res) {
+        Project
+            .find(req.query)
+            .sort({ startDate: -1 })
+            .then(projects => res.json(projects))
+            .catch(err => res.status(422).json(err))
+    },
+    findById: function(req,res) {
+        Project
+            .findById(req.params.id)
+            .then(project => res.json(project))
+            .catch(err => res.status(422).json(err))
+    },
+    create: function(req,res) {
+        Project     
+            .create(req.body)
+            .then(project => res.json(project))
+            .catch(err => res.status(422).json(err))
+            
+    },
+    update: function(req,res) {
+        Project
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(project => res.json(project))
+            .catch(err => res.status(422).json(err))
+    },
+    remove: function(req,res) {
+        Project
+            .findById({ _id: req.params.id })
+            .then(project => project.remove())
+            .then(project => res.json(project))
+            .catch(err => res.status(422).json(err))
+    },
+    //==============================================
+    // PROFILE METHODS
+    findAllProfiles: function(req,res) {
+        Profile
+            .find(req.query)
+            .sort({ startDate: -1 })
+            .then(profiles => res.json(profiles))
+            .catch(err => res.status(422).json(err))
+    },
+    findProfileById: function(req,res) {
+        Profile
+            .findById(req.params.id)
+            .then(profile => res.json(profile))
+            .catch(err => res.status(422).json(err))
+    },
+    createProfile: function(req,res) {
+        Profile     
+            .create(req.body)
+            .then(profile => res.json(profile))
+            .catch(err => res.status(422).json(err))
+    },
+    updateProfile: function(req,res) {
+        Profile
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(profile => res.json(profile))
+            .catch(err => res.status(422).json(err))
+    },
+    removeProfile: function(req,res) {
+        Profile
+            .findById({ _id: req.params.id })
+            .then(profile => profile.remove())
+            .then(profile => res.json(profile))
+            .catch(err => res.status(422).json(err))
+    }       
+}
+
+
+
+
+/*
+const getProfileWithProjects = (userName) => {
+    return Profile.findOne({ userName: userName })
+        .populate('projects')
+        .exec((err, projects) => {
+            console.log('Populated PROFILE: '+projects);
+        });
 };
-module.exports = controller;
+
+const getProjectWithProfiles = (projName) => {
+    return Project.findOne({ projectTitle: projName })
+        .populate('profiles')
+        .exec((err, profiles) => {
+            console.log('Populated PROJECT: '+profiles);
+        });
+}
+
+module.exports = { 
+    getProfileWithProjects, 
+    getProjectWithProfiles
+ }
+ */
